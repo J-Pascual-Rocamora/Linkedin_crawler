4552c4a87fab915753063657bf8f37a1d4069be8
Javier Pascual <javierpascualr@gmail.com>
2018-01-25 16:55:09 +0100

    Fixed settings.py

--- settings.py
+++ settings.py
@@ -1,20 +1,16 @@
 import os
 
-#BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-
 DATABASES = {  
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'linkedin_trial',                      # Or path to database file if using sqlite3.
-        'USER': 'postgres',                      # Not used with sqlite3.
-        'PASSWORD': '1amiaposgresspass',                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
+        'NAME': DATABASE_NAME,                      # Or path to database file if using sqlite3.
+        'USER': DATABASE_USER,                      # Not used with sqlite3.
+        'PASSWORD': DATABASE_PASSWORD,                  # Not used with sqlite3.
+        'HOST': HOST,                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': PORT,                      # Set to empty string for default. Not used with sqlite3.
     }
 }
 
 INSTALLED_APPS = (
     'Linkedin_crawler',
 )
-
-SECRET_KEY = 'REPLACE_ME'
